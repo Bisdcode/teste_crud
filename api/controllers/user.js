55d18a650ee35f7f27d37a83f84db9463fbc1002
Bisdcode <bismarck.sdcode@gmail.com>
2024-07-30 16:53:19 -0300

    implementation of the "post", "put" and "delete" methods.

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -3,11 +3,61 @@ import {db} from "../db.js";
 export const getUsers = (_, res) => {
     const q = 'SELECT * FROM todo';
 
-    //uma query com teste de erro
+    //uma query com 'flag' de erro
     db.query(q, (err, data) => {
         if (err) return res.json(err);
 
     //retornar o ok "status" e os usuarios "json(data)"
         return res.status(200).json(data);
+    });
+};
+
+// Método add user
+
+export const addUser = (req, res) => {
+    const q =
+        "INSERT INTO todo( `text`, `category`) VALUES(?)"; 
+
+    const values = [
+        // req.body.id, autoincrement
+        req.body.text,
+        req.body.category,
+        // req.body.is_completed, para padrão "0"
+    ];
+
+    db.query(q, [values], (err) => {
+        if (err) return res.json(err);
+
+        return res.status(200).json("Tarefa adicionada com sucesso.");
+    });
+};
+
+//Método update
+
+export const updateUser = (req, res) => {
+    const q = "UPDATE todo SET `text`= ?, `category` = ?, WHERE `id` = ?";
+
+    const values = [
+        req.body.text,
+        req.body.category,
+    ];
+
+    db.query(q, [...values, req.params.id], (err) => {
+        if (err) return res.json(err);
+
+        return res.status(200).json("Usuário atualizado com sucesso.");
+    });
+
+};
+
+//Método delete
+
+export const deleteUser = (req, res) => {
+    const q = "DELETE FROM todo WHERE `id` = ?";
+
+    db.query(q, [req.params.id], (err) => {
+        if (err) return res.json(err);
+
+        return res.status(200).json("Usuário deletado com sucesso.");
     });
 };
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,8 +1,15 @@
 import express from "express";
-import { getUsers } from "../controllers/user.js";
+import { getUsers, addUser, updateUser, deleteUser } from "../controllers/user.js";
 
 const router = express.Router()//rota
 
 router.get("/", getUsers);
+
+router.post("/", addUser);
+
+router.put("/", updateUser);
+
+router.delete("/", deleteUser);
+
 
 export default router;
